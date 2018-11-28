7c6137629af438cc8713dfec3f11964322578f00
SESA458036 <SESA458036@WTFRLVSE033216L.eur.gad.schneider-electric.com>
2018-11-28 13:01:44 +0530

    Green Statement added

--- Test/src/JavaDemo.java
+++ Test/src/JavaDemo.java
@@ -4,6 +4,7 @@ public class JavaDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Welcome to Github");
+		System.out.print("green");
 	}
 
 }
